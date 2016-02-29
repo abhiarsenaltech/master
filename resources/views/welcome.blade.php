1ca9bdf4b4252d33c06a5b73a6d376885b0068c4
abhiarsenaltech <abhi.panchal@arsenaltech.com>
2016-02-29 15:18:10 +0530

    second commit

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -8,7 +8,7 @@
                 <div class="panel-heading">Welcome</div>
 
                 <div class="panel-body">
-                   Hello Abhi
+                   Hello
                 </div>
             </div>
         </div>
